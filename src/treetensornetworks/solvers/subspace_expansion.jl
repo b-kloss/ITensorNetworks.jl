c5bb50e844766b060dca77a0af8a77e88280b547
Benedikt <bk2576@columbia.edu>
2023-07-27 17:38:22 -0400

    Refactor general_expander and *expand_core. Moves logic regarding site vs vertex and expand_dir into *expand_core. Also introduces safe permutation of indices when making padded tensor.

--- src/treetensornetworks/solvers/subspace_expansion.jl
+++ src/treetensornetworks/solvers/subspace_expansion.jl
@@ -8,22 +8,28 @@ function general_expander(; expander_backend="none", svd_backend="svd", kwargs..
     maxdim,
     cutoff=1e-10,
     atol=1e-8,
-    expand_dir=+1, # +1 for future direction, -1 for past
+    #expand_dir=+1, # +1 for future direction, -1 for past
     kwargs...,
   ) where {Vert}
 
     ### only on edges
     #(typeof(region)!=NamedEdge{Int}) && return psi, phi, PH
     ### only on verts
-    (typeof(region)==NamedEdge{Int}) && return psi, phi, PH
+    #(typeof(region)==NamedEdge{Int}) && return psi, phi, PH
 
     # determine which expansion method and svd method to use
     if expander_backend == "none"
       return psi, phi, PH
     elseif expander_backend == "full"
-      _expand_core = _full_expand_core
+      _expand_core = _full_expand_core_vertex
+      expand_dir = get(kwargs, :expand_dir, -1)
+      
     elseif expander_backend == "two_site"
       _expand_core = _two_site_expand_core
+      expand_dir = get(kwargs, :expand_dir, +1)
+      #enforce expand_dir in the tested direction
+      @assert expand_dir==1
+      
     else
       error("expander_backend=$expander_backend not recognized (options are \"2-site\" or \"full\")")
     end
@@ -43,10 +49,12 @@ function general_expander(; expander_backend="none", svd_backend="svd", kwargs..
     # (allows to move bond-dimension between different partial QN sectors, if no such truncation occurs distribution of bond-dimensions
     # between different QNs locally is static once bond dimension saturates maxdim.)
     
+
     cutoff_compress = get(kwargs, :cutoff_compress, 1e-12)
     expander_cache = get(kwargs, :expander_cache, Any[])
+    
     to = get(kwargs, :to, Any[])
-
+    #=
     if typeof(region) == NamedEdge{Int} 
       n1,n2 = (src(region),dst(region))
       verts = expand_dir == 1 ? [n1,n2] : [n2,n1]
@@ -68,10 +76,11 @@ function general_expander(; expander_backend="none", svd_backend="svd", kwargs..
       psi[n1] = U
       phi = S*V
     end
+    =#
 
     # subspace expansion
     psi, phi, PH = _expand_core(
-      PH, psi, phi, verts, svd_func; expand_dir, expander_cache, maxdim, cutoff, cutoff_compress, atol, to,
+      PH, psi, phi, region, svd_func; expand_dir, expander_cache, maxdim, cutoff, cutoff_compress, atol, to,
     )
 
     # needed to make sure we expand into the future by multiplying the zero padded bond tensor to the tensor lying in the past
@@ -82,9 +91,11 @@ function general_expander(; expander_backend="none", svd_backend="svd", kwargs..
     # end
 
     # if expansion happens on vertex, return to original form
+    #=
     if typeof(region) != NamedEdge{Int} 
       phi = psi[first(region)] * phi
     end
+    =#
 
     # update environment
     nsites = (region isa AbstractEdge) ? 0 : length(region)
@@ -313,16 +324,43 @@ function _krylov_svd_solve(
 end
 
 function _two_site_expand_core(
-  PH, psi, phi, verts, svd_func; expand_dir, expander_cache, maxdim, cutoff, cutoff_compress, atol, to,
+  PH, psi, phi0, region, svd_func; expand_dir, expander_cache, maxdim, cutoff, cutoff_compress, atol, to,
 )
-  psis = map(n -> psi[n], verts)
+  #enforce expand_dir in the tested direction
+  @assert expand_dir==+1
+  (typeof(region)==NamedEdge{Int}) && return psi, phi0, PH
+  if typeof(region) == NamedEdge{Int} 
+    n1,n2 = (src(region),dst(region))
+    verts = expand_dir == 1 ? [n1,n2] : [n2,n1]
+    psis = map(n -> psi[n], verts)
+    phi=copy(phi0)
+  else
+
+    ## kind of hacky - only works for mps. More general?
+    n1 = first(region)
+    if direction == 1 
+      n2 = expand_dir == 1 ? n1 - 1 : n1+1
+    else
+      n2 = expand_dir == 1 ? n1 + 1 : n1-1
+    end
+    (n2 < 1 || n2 > length(vertices(psi))) && return psi,phi0,PH
+    verts = [n1,n2]
+    psis = map(n -> psi[n], verts)
+    ## bring it into the same functional form used for the Named-Edge case by doing an svd
+    left_inds = uniqueinds(psi[n1], psi[n2])
+    U, S, V = svd(psis[findall(verts.==n1)[]], left_inds; lefttags=tags(commonind(psi[n1],psi[n2])), righttags=tags(commonind(psi[n1],psi[n2])))
+    psis[findall(verts.==n1)[]]= U
+    phi = S*V
+  end
+  #psis = map(n -> psi[n], verts)
+ 
   old_linkdim = dim(commonind(first(psis), phi))
 
   PH = set_nsite(PH, 2)
   PH = position(PH, psi, verts)
 
   # don't expand if we are already at maxdim
-  (old_linkdim >= maxdim) && return psi, phi, PH
+  (old_linkdim >= maxdim) && return psi, phi0, PH
 
   linkinds = map(psi -> commonind(psi, phi), psis)
 
@@ -332,7 +370,7 @@ function _two_site_expand_core(
   end
   
   # if nullspace is empty (happen's for product states with QNs)
-  sum(norm.(nullVecs) .== 0) > 0 && return psi, phi, PH
+  sum(norm.(nullVecs) .== 0) > 0 && return psi, phi0, PH
 
   ## build environments
   g = underlying_graph(PH)
@@ -348,7 +386,7 @@ function _two_site_expand_core(
   envMap = ITensors.ITensorNetworkMaps.ITensorNetworkMap([last(nullVecs),last(envs),phi,first(envs),first(nullVecs)], outinds, ininds)
   
   U,S,V = svd_func(envMap, outinds; maxdim=maxdim-old_linkdim, cutoff=cutoff)
-  isnothing(U) && return psi, phi, PH
+  isnothing(U) && return psi, phi0, PH
 
   @assert dim(commonind(U, S)) ≤ maxdim-old_linkdim
 
@@ -370,13 +408,15 @@ function _two_site_expand_core(
   @assert sum(findmax.(dim.(new_inds))[1] .> maxdim) == 0
 
   # zero-pad bond-tensor (the orthogonality center)
+
+  phi_indices = replace(inds(phi), (commonind(phi,psis[n]) => dag(new_inds[n]) for n in 1:2)...)
+  #phi_indices = replace(phi_indices, commonind(phi,psis[2]) => dag(new_inds[2]))
+  
+ 
   if hasqns(phi)
-    phi_indices = (dag(last(new_inds)),dag(first(new_inds)))
     new_phi=ITensor(eltype(phi),flux(phi), phi_indices...)
     fill!(new_phi,0.0)
   else
-    phi_indices = (dag(first(new_inds)),dag(last(new_inds)))
-    # phi_indices = (dag(last(new_inds)),dag(first(new_inds)))
     new_phi = ITensor(eltype(phi), phi_indices...)
   end
 
@@ -385,23 +425,143 @@ function _two_site_expand_core(
     !iszero(v) && (return new_phi[I]=v)
   end
 
-  old_twosite_tensor = psi[last(verts)]*phi*psi[first(verts)]
+  old_twosite_tensor = first(psis)*phi*last(psis)
 
   for (v,new_psi,C) in zip(verts,new_psis,combiners)
     psi[v] = noprime(new_psi*C)
   end
 
   new_phi = dag(first(combiners)) * new_phi * dag(last(combiners))
-
-  @assert norm(psi[last(verts)]*new_phi*psi[first(verts)] - old_twosite_tensor) < eps(Float64)
- 
+  
+  @assert norm(psi[last(verts)]*new_phi*psi[first(verts)] - old_twosite_tensor) < 50*eps(Float64)
+  
+  if typeof(region) != NamedEdge{Int}
+    psi[n1]*=new_phi
+    new_phi=psi[n1]
+  end
   return psi, new_phi, PH
+  
 end
 
 function _full_expand_core_vertex(
-  PH, psi, phi, verts, svd_func; expand_dir, expander_cache, maxdim, cutoff, cutoff_compress, atol, to,
+  PH, psi, phi, region, svd_func; expand_dir, expander_cache, maxdim, cutoff, cutoff_compress, atol, to,
 )
+  #enforce expand_dir in the tested direction
+  @assert expand_dir===1
+  ### only on edges
+  #(typeof(region)!=NamedEdge{Int}) && return psi, phi, PH
+  ### only on verts
+  (typeof(region)==NamedEdge{Int}) && return psi, phi, PH
+  ## kind of hacky - only works for mps. More general?
+  n1 = first(region)
+  #expand_dir=-1
+  if direction == 1 
+    n2 = expand_dir == 1 ? n1 - 1 : n1+1
+  else
+    n2 = expand_dir == 1 ? n1 + 1 : n1-1
+  end
+  (n2 < 1 || n2 > length(vertices(psi))) && return psi,phi,PH
+  verts = [n1,n2]
+  
+  if isempty(expander_cache)
+    @warn("building environment of H^2 from scratch!")
+  
+    g = underlying_graph(PH.H)
+    H = vertex_data(data_graph(PH.H))
+
+    H_dag = swapprime.(prime.(dag.(H)), 1,2, tags = "Site")
+    H2_vd= replaceprime.(map(*, H, H_dag), 2 => 1)
+    H2_ed = edge_data(data_graph(PH.H))
+
+    H2 = TTN(ITensorNetwork(DataGraph(g, H2_vd, H2_ed)), PH.H.ortho_center)
+    PH2 = ProjTTN(H2)
+    PH2 = set_nsite(PH2, 2)
+
+    push!(expander_cache, PH2)
+  end
+
+  PH2 = expander_cache[1]
+  n1 = verts[2]
+  n2 = verts[1]
+  
+  ###do we really need to make the environments two-site?
+  ###look into how ProjTTN works
+  PH2 = position(PH2, psi, [n1,n2])
+  PH = position(PH, psi, [n1,n2])
+
+  psi1 = psi[n1]  
+  psi2 = psi[n2] #phi
+  old_linkdim = dim(commonind(psi1, psi2))
+
+  # don't expand if we are already at maxdim
+  ## make more transparent that this is not the normal maxdim arg but maxdim_expand
+  old_linkdim >= maxdim && return psi, phi, PH
+
+  # compute nullspace to the left and right 
+  linkind_l = commonind(psi1, psi2)
+  ### ToDo: implement a composite LinearMap for Nullspace projector
+  nullVec = nullspace(psi1, linkind_l; atol=atol)
+
+  # if nullspace is empty (happen's for product states with QNs)
+  ## ToDo implement norm or equivalent for a generic LinearMap (i guess via sampling a random vector)
+  norm(nullVec) == 0.0 && return psi, phi, PH
+
+
+  ## compute both environments
+  g = underlying_graph(PH)
+
+  env1 = noprime(mapreduce(*, [v => n1 for v in neighbors(g, n1) if v != n2]; init = psi1) do e
+    return PH.environments[NamedEdge(e)]
+  end *PH.H[n1]
+  )
+  env2 = mapreduce(*, [v => n2 for v in neighbors(g, n2) if v != n1]; init = psi2) do e
+    return PH2.environments[NamedEdge(e)]
+  end * PH2.H[n2]*prime(dag(psi2))
+
+  outinds = uniqueinds(nullVec,psi1)
+  ininds = dag.(outinds)
+  envMap = ITensors.ITensorNetworkMaps.ITensorNetworkMap([prime(dag(nullVec)),prime(dag(env1)),env2,env1,nullVec] , outinds, ininds)
+
+  # svd-decomposition
+  U,S,_= svd_func(envMap, outinds; maxdim=maxdim-old_linkdim, cutoff=cutoff)
+  isnothing(U) && return psi,phi,PH
+
+  @assert dim(commonind(U, S)) ≤ maxdim
+
+  nullVec = nullVec*dag(U)
+
+  # expand current site tensors
+  new_psi1, new_ind1 = ITensors.directsum(
+    psi1 => uniqueinds(psi1, nullVec), dag(nullVec) => uniqueinds(dag(nullVec), psi1); tags=(tags(commonind(psi1,phi)),)
+  )
+  new_ind1 = new_ind1[1]
+  @assert dim(new_ind1) <= maxdim
+
+  Cl = combiner(new_ind1; tags=tags(new_ind1), dir=dir(new_ind1))
+
+  phi_indices = replace(inds(phi), commonind(phi,psi1) => dag(new_ind1))
+ 
+  if hasqns(phi)
+    new_phi=ITensor(eltype(phi),flux(phi), phi_indices...)
+    fill!(new_phi,0.0)
+  else
+    new_phi = ITensor(eltype(phi), phi_indices...)
+  end
+
+  map(eachindex(phi)) do I
+    v = phi[I]
+    !iszero(v) && (return new_phi[I]=v)
+  end
+
+  #old_twosite_tensor=phi*psi[n1]
+  psi[n1] = noprime(new_psi1*Cl)
+  new_phi = dag(Cl)*new_phi
+  #@show norm(new_phi*psi[n1] - old_twosite_tensor) 
+  #@assert norm(new_phi*psi[n1] - old_twosite_tensor) < 50*eps(Float64)
+
+  return psi, new_phi, PH
 end
+
 
 function _full_expand_core(
   PH, psi, phi, verts, svd_func; expand_dir, expander_cache, maxdim, cutoff, cutoff_compress, atol, to,
