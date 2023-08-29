8bf178bb5bca6f701955c137b39557909244699c
b-kloss <bene.kloss@gmail.com>
2023-08-29 17:23:02 -0400

    Fix issue with call of flux on TTNS in _full_expand_vertex. Remove svd backend from subspace_expansion.jl

--- src/treetensornetworks/solvers/subspace_expansion.jl
+++ src/treetensornetworks/solvers/subspace_expansion.jl
@@ -60,233 +60,13 @@
 #   return expander
 # end
 
-function _svd_solve_normal(
-  envMap, left_ind; maxdim, cutoff 
-)
-  M = ITensors.ITensorNetworkMaps.contract(envMap)
- #@show inds(M)
-  #@show norm(M)
-  norm(M) ≤ eps(Float64) && return nothing,nothing,nothing
 
-  U,S,V = svd(
-    M,
-    left_ind;
-    maxdim,
-    cutoff=cutoff,
-    use_relative_cutoff=false,
-    use_absolute_cutoff=true,
-  )
-
-  vals = diag(array(S))
-  (length(vals) == 1 && vals[1]^2 ≤ cutoff) && return nothing,nothing,nothing
-
-  return U,S,V
-end
 
 #_svd_solve_normal(envMap,envMapDag, left_ind;kwargs...)=svd_solve_normal(envMap, left_ind;maxdim=get(::maxdim,kwargs),cutoff=cutoff)
 #_svd_solve_normal(T,envMap,envMapDag, left_ind;kwargs...)=svd_solve_normal(envMap, left_ind;maxdim=maxdim,cutoff=cutoff)
 
 
-function _build_USV_without_QN(vals, lvecs, rvecs)
-  # attach trivial index to left/right eigenvectors to take directsum over it
-  lvecs = map(lvecs) do lvec 
-    dummy_ind = Index(1; tags="u") 
-    return ITensor(array(lvec), inds(lvec)..., dummy_ind) => dummy_ind
-  end
 
-  rvecs = map(rvecs) do rvec 
-    dummy_ind = Index(1; tags="v")
-    return ITensor(array(rvec), inds(rvec)..., dummy_ind) => dummy_ind
-  end
-
-  ### perform directsum
-  U,_ = reduce((x,y) -> ITensors.directsum(
-    x, y; tags="u", 
-  ), lvecs[2:end]; init=lvecs[1])
-
-  V,_ = reduce((x,y) -> ITensors.directsum(
-    x, y; tags="v",
-  ), rvecs[2:end]; init=rvecs[1])
-
-  S = ITensors.diagITensor(vals, filter(v->hastags(v, "u"), inds(U)), filter(v->hastags(v, "v"), inds(V)))
-
-  return U,S,V
-end
-
-function _build_USV_with_QN(vals_col, lvecs_col, rvecs_col, remainingSpaces;envflux)
-  # attach trivial index to left/right eigenvectors to take directsum over it
-  lvecs_col = map(zip(remainingSpaces,lvecs_col)) do (s,lvecs)
-    return map(lvecs) do lvec 
-      dummy_ind = Index(Pair(first(s),1); tags="u", dir=ITensors.In)
-      return ITensor(array(lvec), inds(lvec)..., dummy_ind) => dummy_ind
-    end
-  end
-
-  rvecs_col = map(zip(remainingSpaces,rvecs_col)) do (s,rvecs)
-    return map(rvecs) do rvec
-      rvec=rvec 
-      dummy_ind = Index(Pair(envflux-first(s),1); tags="v", dir=ITensors.In)
-      return ITensor(array(rvec), inds(rvec)..., dummy_ind) => dummy_ind
-    end
-  end
-
-  ### perform directsum of left/right eigenvectors for each sector
-  lvecs_col = map(lvecs_col) do lvecs
-    l, inds_l = reduce((x,y) -> ITensors.directsum(
-      x, y; tags="u",
-    ), lvecs[2:end]; init=lvecs[1])
-    length(lvecs) == 1 && return l => inds_l
-    C = combiner(inds_l; tags="u", dir=dir(inds_l))
-    l = l*C
-    return l => commonind(l,C)
-  end
-
-  rvecs_col = map(rvecs_col) do rvecs
-    r, inds_r = reduce((x,y) -> ITensors.directsum(
-      x, y; tags="v",
-    ), rvecs[2:end]; init=rvecs[1])
-    length(rvecs) == 1 && return r => inds_r
-    C = combiner(inds_r; tags="v", dir=dir(inds_r))
-    r = r*C
-    return r => commonind(r,C)
-  end
-
-  ### perform directsum over all sectors
-  U,_ = reduce((x,y) -> ITensors.directsum(
-    x, y; tags="u" 
-  ), lvecs_col[2:end]; init=lvecs_col[1])
-
-  V,_ = reduce((x,y) -> ITensors.directsum(
-    x, y; tags="v",
-  ), rvecs_col[2:end]; init=rvecs_col[1])
-
-  S = ITensors.diagITensor(vcat(vals_col...), filter(v->hastags(v, "u"), dag(inds(U))), filter(v->hastags(v, "v"), dag(inds(V))))
-  return U,S,V
-end
-
-function _truncate_blocks!(d, vals_col, lvecs_col, rvecs_col, remainingSpaces, cutoff, maxdim) 
-  d .= d .^ 2
-  sort!(d; rev=true)
-  docut=0.0
-
-  ## implements what truncate! is supposed to do
-  if length(d)==1 && d[1]<cutoff
-     docut=d[1]+eps(eltype(d))
-  elseif length(d)>1
-    pos=argmin(d .>= cutoff)
-    docut=d[min(maxdim+1,pos)]
-  end
-  
-  dropblocks = Int[]
-
-  for (n,vals) in enumerate(vals_col)
-    full_dim = length(vals)
-    blockdim = 0
-    val = vals[blockdim + 1]^2
-    while blockdim + 1 ≤ full_dim && val > docut
-      blockdim += 1
-      (blockdim + 1 ≤ full_dim) && (val = vals[blockdim + 1]^2)
-    end
-
-    if blockdim == 0
-      push!(dropblocks,n)
-    else
-      vals_col[n]  = vals[1:blockdim]
-      lvecs_col[n] = lvecs_col[n][1:blockdim]
-      rvecs_col[n] = rvecs_col[n][1:blockdim]
-    end
-  end
-
-  deleteat!(vals_col,  dropblocks)
-  deleteat!(lvecs_col, dropblocks)
-  deleteat!(rvecs_col, dropblocks)
-  deleteat!(remainingSpaces,  dropblocks)
-end
-
-function _krylov_svd_solve(T,
-  envMap,envMapDag, left_ind; flux=flux(contract(envMap)),maxdim, cutoff,# kwargs...
-)
-  maxdim = min(maxdim, 15)
-  krylov_args = (tol = cutoff, krylovdim = maxdim, maxiter = 1)
-  if !hasqns(left_ind)
-    trial = randomITensor(eltype(envMap), left_ind)
-    trial = trial / norm(trial)
-
-    @timeit_debug timer "krylov check" begin
-      _kkit_init_check(trial,envMapDag,envMap) || (return nothing,nothing,nothing)
-    end
-
-    try
-      
-      @timeit_debug timer "krylov svd solve" begin
-        vals, lvecs, rvecs, info = KrylovKit.svdsolve(
-          (x -> envMap*x, y -> envMapDag*y), trial; krylov_args...,
-        )
-      end
-    catch e 
-      @show e
-      return _svd_solve_normal(envMap, left_ind; maxdim, cutoff)
-    end
-
-    ## cutoff unnecessary values and expand the resulting vectors by a dummy index
-    @timeit_debug timer "build USV from Krylov vecs" begin
-      vals = filter(v->v^2≥cutoff, vals)[1:min(maxdim,end)]
-      (length(vals) == 0) && return nothing,nothing,nothing
-      lvecs = lvecs[1:min(length(vals),end)]
-      rvecs = rvecs[1:min(length(vals),end)]
-      U,S,V = _build_USV_without_QN(vals, lvecs, rvecs)
-    end
-
-  else
-    vals_col  = Any[]
-    lvecs_col = Any[]
-    rvecs_col = Any[]
-    d = Vector{real(T)}()
-    remainingSpaces = Any[]
-
-    for s in space(left_ind[1])
-      last(s)==0 && continue
-
-      theqn=first(s)
-      trial = randomITensor(T, theqn, left_ind)
-      adjtrial=envMapDag*trial
-      trial2=envMap*adjtrial
-      
-      if size(storage(adjtrial))==(0,) || size(storage(trial2))==(0,)
-        continue
-      elseif ! _kkit_init_check(trial, envMapDag,envMap)
-        continue
-      end
-      adjtrial=nothing
-      trial2=nothing
-      try
-        @timeit_debug timer "krylov svd solve" begin
-          vals, lvecs, rvecs, info = KrylovKit.svdsolve(
-            (x -> envMap*x, y -> envMapDag*y), trial; krylov_args...,
-          )
-        end
-      catch e 
-        @show e
-        return _svd_solve_normal(envMap, left_ind; maxdim, cutoff)
-      end
-      push!(vals_col,  vals)
-      push!(lvecs_col, lvecs)
-      push!(rvecs_col, conj.(dag.(rvecs)))  ###is the conjugate here justified or not?  
-      push!(remainingSpaces, s)
-      append!(d, vals)
-    end
-    (length(d) == 0) && return nothing,nothing,nothing
-
-    @timeit_debug timer "build USV from Krylov vecs" begin
-      _truncate_blocks!(d, vals_col, lvecs_col, rvecs_col, remainingSpaces, cutoff, maxdim)
-
-      (length(vals_col) == 0) && return nothing,nothing,nothing
-      U,S,V = _build_USV_with_QN(vals_col, lvecs_col, rvecs_col, remainingSpaces; envflux=flux)
-    end
-  end
-
-  return U,S,V
-end
 
 function implicit_nullspace(A, linkind)
   ###only works when applied in the direction of the environment tensor, not the other way (due to use of ITensorNetworkMap which is not reversible)
@@ -438,6 +218,7 @@ function _full_expand_core_vertex(
   (typeof(region)==NamedEdge{Int}) && return psi, phi, PH
   ## kind of hacky - only works for mps. More general?
   n1 = first(region)
+  theflux=flux(psi[n1])
   #expand_dir=-1
   if direction == 1 
     n2 = expand_dir == 1 ? n1 - 1 : n1+1
@@ -527,7 +308,7 @@ function _full_expand_core_vertex(
     if svd_func==ITensorNetworks._svd_solve_normal
       U,S,_ = svd_func(envMap, uniqueinds(inds(cout),outinds); maxdim=maxdim-old_linkdim, cutoff=cutoff)
     else
-      U,S,_= svd_func(eltype(envMap),envMap,envMapDag,uniqueinds(cout,outinds); flux=flux(psi), maxdim=maxdim-old_linkdim, cutoff=cutoff)
+      U,S,_= svd_func(eltype(envMap),envMap,envMapDag,uniqueinds(cout,outinds); flux=theflux, maxdim=maxdim-old_linkdim, cutoff=cutoff)
     end
   end
   isnothing(U) && return psi,phi,PH
